ba8674df90de665b0df3331802efa8e8fc5cdc56
Alex Gonzalez <7432182+luinrandir@users.noreply.github.com>
2024-01-19 00:28:27 -0800

    feat: added new nvim config with lazy.nvim, also added chezmoiignore file for README

---
+++ private_dot_config/nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,16 @@
+return {
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	config = function()
+		local configs = require("nvim-treesitter.configs")
+		configs.setup({
+			ensure_installed = {"javascript", "typescript", "html", "css", "scss", "bash", "toml", "lua"},
+			sync_install = false,
+			auto_install = true,
+			highlight = {
+				enable = true,
+				additional_vim_regex_highlighting = false,
+			},
+		})
+	end
+}
---
+++ private_dot_config/nvim/lua/vim-maps.lua
@@ -0,0 +1,25 @@
+--The Primeagen's inipired/copied commands
+    
+vim.g.mapleader = " "
+
+--Move highlighted lines
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '>-2<CR>gv=gv")
+
+--Keep cursor in middle while jumping and searching
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+--No Q
+vim.keymap.set("n", "Q", "<nop>")
+
+--Format Stuff
+vim.keymap.set("n", "<leader>lf", ":lua vim.lsp.buf.format()<CR>")
+
+-- Regex Stuff
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+
+-- Chmod current file
+vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
