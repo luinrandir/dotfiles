203e500e0b62bcd2d01aa982a5f61d0709ab0c9b
Alex Gonzalez <7432182+luinrandir@users.noreply.github.com>
2024-01-20 12:23:06 -0800

    chore(autocomplete): added more autocomplete functionality for buffer, path, and lua api

--- private_dot_config/nvim/lua/plugins/autocomplete.lua
+++ private_dot_config/nvim/lua/plugins/autocomplete.lua
@@ -1,7 +1,8 @@
 return {
-    {
-        "hrsh7th/cmp-nvim-lsp",
-    },
+    { "hrsh7th/cmp-buffer" },
+    { "hrsh7th/cmp-path" },
+    { "hrsh7th/cmp-nvim-lua" },
+    { "hrsh7th/cmp-nvim-lsp" },
     {
         "L3MON4D3/LuaSnip",
         dependencies = { "saadparwaiz1/cmp_luasnip", "rafamadriz/friendly-snippets" },
@@ -34,9 +35,10 @@ return {
                 }),
                 sources = cmp.config.sources({
                     { name = "nvim_lsp" },
-                    { name = "luasnip" }, -- For luasnip users.
-                }, {
+                    { name = "luasnip" },
+                    { name = "nvim_lua" },
                     { name = "buffer" },
+                    { name = "path" },
                 }),
             })
         end,
