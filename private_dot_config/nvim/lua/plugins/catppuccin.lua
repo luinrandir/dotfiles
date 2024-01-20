4bda9609942cf388967b76b5dabcfab69be30f69
Alex Gonzalez <7432182+luinrandir@users.noreply.github.com>
2024-01-19 21:51:09 -0800

    chore: connected none-ls tools and changed bg color for normalfloat windows

--- private_dot_config/nvim/lua/plugins/catppuccin.lua
+++ private_dot_config/nvim/lua/plugins/catppuccin.lua
@@ -6,7 +6,7 @@ return {
 		vim.cmd.colorscheme("catppuccin")
 		vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 		vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
-		vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+--		vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 		vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" })
 	end,
 }
