4a40fff5d32aa685e827bc09805fbd4a806bdf71
Alex Gonzalez <7432182+luinrandir@users.noreply.github.com>
2024-02-04 21:27:21 -0800

    feat: added comform and nvim-lint to replace none-ls

---
+++ private_dot_config/nvim/lua/plugins/conform.lua
@@ -0,0 +1,40 @@
+return {
+	"stevearc/conform.nvim",
+	lazy = true,
+	event = { "BufReadPre", "BufNewFile" },
+	config = function()
+		local conform = require("conform")
+		conform.setup({
+			formatters_by_ft = {
+				javascript = { "prettier" },
+				typescript = { "prettier" },
+				javascriptreact = { "prettier" },
+				typescriptreact = { "prettier" },
+				css = { "prettier" },
+				scss = { "prettier" },
+				html = { "prettier" },
+				json = { "prettier" },
+				yaml = { "prettier" },
+				toml = { "prettier" },
+				markdown = { "prettier" },
+				lua = { "stylua" },
+				go = { "gofumpt", "goimports-reviser", "golines" },
+			},
+			format_on_save = {
+				lsp_fallback = true,
+				async = false,
+				timeout_ms = 1000,
+			},
+		})
+
+		--Format Stuff
+		vim.keymap.set({ "n", "v" }, "<leader>lf", function()
+			conform.format({
+
+				lsp_fallback = true,
+				async = false,
+				timeout_ms = 1000,
+			})
+		end, { desc = "Format file or range when in visual mode" })
+	end,
+}
