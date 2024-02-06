4ee9bbe1706a26353a8089a967f47142d5df11d2
Alex Gonzalez <7432182+luinrandir@users.noreply.github.com>
2024-02-06 15:36:02 -0800

    refactor: removed telescope-ui-select plugin

--- private_dot_config/nvim/lua/plugins/telescope.lua
+++ private_dot_config/nvim/lua/plugins/telescope.lua
@@ -9,17 +9,4 @@ return {
 			vim.keymap.set("n", "<leader>ps", builtin.live_grep, {})
 		end,
 	},
-	{
-		"nvim-telescope/telescope-ui-select.nvim",
-		config = function()
-			require("telescope").setup({
-				extensions = {
-					["ui-select"] = {
-						require("telescope.themes").get_dropdown({}),
-					},
-				},
-			})
-			require("telescope").load_extension("ui-select")
-		end,
-	},
 }
