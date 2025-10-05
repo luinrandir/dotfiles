4afe3addcaee571a1d109dfe59f2a96dbc471a4a
Alex Gonzalez <7432182+luinrandir@users.noreply.github.com>
2025-10-05 11:08:26 -0700

    Fixing Java LSP....again...
    Fixing it again...

--- private_dot_config/nvim/lua/plugins/mason.lua
+++ private_dot_config/nvim/lua/plugins/mason.lua
@@ -1,18 +1,23 @@
 return {
-	{
-		"mason-org/mason.nvim",
-		opts = {},
-	},
-	{
-		"mason-org/mason-lspconfig.nvim",
-		dependencies = {
-			"neovim/nvim-lspconfig",
-			"mason-org/mason.nvim",
-		},
-		opts = {
-			ensure_installed = {
-				"lua_ls",
-			},
-		},
-	},
+  {
+    "mason-org/mason.nvim",
+    opts = {
+      registries = {
+        'github:nvim-java/mason-registry',
+        'github:mason-org/mason-registry',
+      },
+    },
+  },
+  {
+    "mason-org/mason-lspconfig.nvim",
+    dependencies = {
+      "neovim/nvim-lspconfig",
+      "mason-org/mason.nvim",
+    },
+    opts = {
+      ensure_installed = {
+        "lua_ls",
+      },
+    },
+  },
 }
