ab7e2c503e5c108976a603d7d13a9bb7a8eda22b
Alex Gonzalez <7432182+luinrandir@users.noreply.github.com>
2025-10-04 19:30:04 -0700

    Snacks config updated
    Updated to turn off the explorer, but then switched it back.

--- private_dot_config/nvim/lua/plugins/snacks.lua
+++ private_dot_config/nvim/lua/plugins/snacks.lua
@@ -1,39 +1,46 @@
 return {
-	"folke/snacks.nvim",
-	priority = 1000,
-	lazy = false,
-	---@type snacks.Config
-	opts = {
-		-- your configuration comes here
-		-- or leave it empty to use the default settings
-		-- refer to the configuration section below
-		bigfile = { enabled = true },
-		dashboard = { enabled = true },
-		explorer = { enabled = true },
-		indent = { enabled = true },
-		input = { enabled = true },
-		picker = { enabled = true },
-		notifier = { enabled = true },
-		quickfile = { enabled = true },
-		scope = { enabled = true },
-		scroll = { enabled = true },
-		statuscolumn = { enabled = true },
-		words = { enabled = true },
-	},
-	keys = {
-		{
-			"<leader>pv",
-			function()
-				Snacks.explorer()
-			end,
-			desc = "File Explorer",
-		},
-		{
-			"<leader>sh",
-			function()
-				Snacks.notifier.show_history()
-			end,
-			desc = "Show Notifications",
-		},
-	},
+  "folke/snacks.nvim",
+  priority = 1000,
+  lazy = false,
+  ---@type snacks.Config
+  opts = {
+    -- your configuration comes here
+    -- or leave it empty to use the default settings
+    -- refer to the configuration section below
+    bigfile = { enabled = true },
+    dashboard = { enabled = true },
+    explorer = { enabled = true },
+    indent = { enabled = true },
+    input = { enabled = true },
+    picker = {
+      enabled = true,
+      sources = {
+        explorer = {
+          -- auto_close = true,
+        }
+      }
+    },
+    notifier = { enabled = true },
+    quickfile = { enabled = true },
+    scope = { enabled = true },
+    scroll = { enabled = true },
+    statuscolumn = { enabled = true },
+    words = { enabled = true },
+  },
+  keys = {
+    {
+      "<leader>pv",
+      function()
+        Snacks.explorer()
+      end,
+      desc = "File Explorer",
+    },
+    {
+      "<leader>sh",
+      function()
+        Snacks.notifier.show_history()
+      end,
+      desc = "Show Notifications",
+    },
+  },
 }
