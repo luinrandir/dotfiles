9ed151848d451b954212ebd37444c008e86007fe
Alex Gonzalez <7432182+luinrandir@users.noreply.github.com>
2026-05-27 13:52:07 -0700

    Formatting and Linting added
    Added conform and nvim-lint to nvim configuration

--- dot_config/nvim/lua/plugins/lsp/blink-cmp.lua
+++ dot_config/nvim/lua/plugins/lsp/blink-cmp.lua
@@ -1,33 +1,33 @@
 return {
-  {
-  'saghen/blink.cmp',
-  -- optional: provides snippets for the snippet source
-  dependencies = { 'rafamadriz/friendly-snippets'},
-  -- use a release tag to download pre-built binaries
-  version = '1.*',
-  opts = {
-    keymap = { preset = 'default' },
+	{
+		"saghen/blink.cmp",
+		-- optional: provides snippets for the snippet source
+		dependencies = { "rafamadriz/friendly-snippets" },
+		-- use a release tag to download pre-built binaries
+		version = "1.*",
+		opts = {
+			keymap = { preset = "default" },
 
-    appearance = {
-      nerd_font_variant = 'mono'
-    },
+			appearance = {
+				nerd_font_variant = "mono",
+			},
 
-    completion = { documentation = { auto_show = false } },
+			completion = { documentation = { auto_show = false } },
 
-    sources = {
-      default = { 'lazydev', 'lsp', 'path', 'snippets', 'buffer' },
-      providers = {
-        lazydev = {
-          name = 'LazyDev',
-          module = 'lazydev.integrations.blink',
-        }
-      }
-    },
+			sources = {
+				default = { "lazydev", "lsp", "path", "snippets", "buffer" },
+				providers = {
+					lazydev = {
+						name = "LazyDev",
+						module = "lazydev.integrations.blink",
+					},
+				},
+			},
 
-    signature = {enabled = true},
+			signature = { enabled = true },
 
-    fuzzy = { implementation = "prefer_rust_with_warning" }
-  },
-  opts_extend = { "sources.default" }
-}
+			fuzzy = { implementation = "prefer_rust_with_warning" },
+		},
+		opts_extend = { "sources.default" },
+	},
 }
--- dot_config/nvim/lua/plugins/lsp/mason.lua
+++ dot_config/nvim/lua/plugins/lsp/mason.lua
@@ -1,42 +1,85 @@
 return {
-    {
-    "williamboman/mason-lspconfig.nvim",
-    opts = {
-      -- list of servers for mason to install
-      ensure_installed = {
-        "lua_ls",
-      },
-  },
-    dependencies = {
-      {
-        "williamboman/mason.nvim",
-        opts = {
-          ui = {
-            icons = {
-              package_installed = "✓",
-              package_pending = "➜",
-              package_uninstalled = "✗",
-            },
-          },
-        },
-      },
-        {
-          "neovim/nvim-lspconfig",
-          dependencies = {
-            {
-    "folke/lazydev.nvim",
-    ft = "lua", -- only load on lua files
-    opts = {
-      library = {
-        -- See the configuration section for more details
-        -- Load luvit types when the `vim.uv` word is found
-        { path = "${3rd}/luv/library", words = { "vim%.uv" } },
-      },
-    },
-  },
-          }
-      },
-    },
-  },
-}
+	{
+		"williamboman/mason-lspconfig.nvim",
+		opts = {
+			-- list of servers for mason to install
+			ensure_installed = {
+				"lua_ls",
+				"gopls",
+			},
+		},
+		dependencies = {
+			{
+				"williamboman/mason.nvim",
+				opts = {
+					ui = {
+						icons = {
+							package_installed = "✓",
+							package_pending = "➜",
+							package_uninstalled = "✗",
+						},
+					},
+				},
+			},
+			{
+				"neovim/nvim-lspconfig",
+				dependencies = {
+					{
+						"folke/lazydev.nvim",
+						ft = "lua", -- only load on lua files
+						opts = {
+							library = {
+								-- See the configuration section for more details
+								-- Load luvit types when the `vim.uv` word is found
+								{ path = "${3rd}/luv/library", words = { "vim%.uv" } },
+							},
+						},
+					},
+				},
+				config = function()
+					vim.diagnostic.config({
+						virtual_lines = true,
+						underline = true,
+					})
+				end,
+			},
+		},
+	},
+	{
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+		opts = {
+			ensure_installed = {
+				--Catch-all formatter
+				"prettier", -- prettier formatter
 
+				--JavaScript/TypeScript linter
+				"eslint_d",
+
+				--Go formatter & linter
+				"gofumpt",
+				"goimports-reviser",
+				"golines",
+				"golangci-lint",
+
+				--CSS/SCSS linter
+				"stylelint",
+
+				--Lua formatter
+				"stylua",
+
+				--Python formatter & linter
+				"isort",
+				"black",
+				"pylint",
+				"ruff",
+				"mypy",
+
+				--Bash linter
+				"shellcheck",
+			},
+		},
+		dependencies = {
+			"williamboman/mason.nvim",
+		},
+	},
+}
