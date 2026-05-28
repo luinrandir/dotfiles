fe7839f868d3782eb461324e32807eedde0babf2
Alex Gonzalez <7432182+luinrandir@users.noreply.github.com>
2026-05-27 19:57:22 -0700

    nvim transparency
    fixed transparency for nvim catppuccin theme

--- dot_config/nvim/lua/plugins/catppuccin.lua
+++ dot_config/nvim/lua/plugins/catppuccin.lua
@@ -2,11 +2,14 @@ return {
 	"catppuccin/nvim",
 	name = "catppuccin",
 	priority = 1000,
-	config = function()
+	opts = {
+		transparent_background = true,
+		float = {
+			transparent = true,
+		},
+	},
+	config = function(_, opts)
+		require("catppuccin").setup(opts)
 		vim.cmd.colorscheme("catppuccin")
-		-- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-		-- vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
---		vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-		-- vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" })
 	end,
 }
